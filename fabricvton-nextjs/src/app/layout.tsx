3eb99ef9a0c8cab7391c15217d4b76cb74268feb
aaditya3301 <aadityasinghal77@gmail.com>
2026-05-01 03:34:34 +0530

    ga added

--- fabricvton-nextjs/src/app/layout.tsx
+++ fabricvton-nextjs/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -28,6 +29,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="antialiased">
@@ -37,6 +40,7 @@ export default function RootLayout({
         </SmoothScroll>
         <Toaster position="bottom-right" richColors />
       </body>
+      {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
     </html>
   );
 }
